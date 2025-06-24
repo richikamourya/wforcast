3da7b81b8239d0b1eb6d6689f1a6676d925ef1f1
richikamourya <richikamourya@gmail.com>
2025-06-24 16:23:36 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,41 @@
+const apiKey = "19b7154364ade3a2cfaeb0e5c18fbf95"; // Replace with your OpenWeatherMap API key
+
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lon = position.coords.longitude;
+      let lat = position.coords.latitude;
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          weatherReport(data);
+        });
+    });
+  }
+});
+
+function searchByCity() {
+  const place = document.getElementById("input").value;
+  const urlSearch = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}`;
+
+  fetch(urlSearch)
+    .then((res) => res.json())
+    .then((data) => {
+      weatherReport(data);
+    });
+
+  document.getElementById("input").value = "";
+}
+
+function weatherReport(data) {
+  document.getElementById("city").innerText = `${data.name}, ${data.sys.country}`;
+  document.getElementById("temperature").innerText =
+    Math.floor(data.main.temp - 273.15) + " °C";
+  document.getElementById("clouds").innerText = data.weather[0].description;
+
+  const icon = data.weather[0].icon;
+  document.getElementById("img").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+}
